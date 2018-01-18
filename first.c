b5f558aa82fac5ee2d001dc36da356c2c9fb11d2
ektasrivastava31 <35468369+ektasrivastava31@users.noreply.github.com>
2018-01-18 18:59:53 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter the numbers");
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("positive no.");
+		
+		}
+	else
+	if(n<0)
+	{
+		printf("negative no.");
+	}
+	else
+	{
+		printf("Zero");
+	}
+}
