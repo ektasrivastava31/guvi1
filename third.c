0bf81f94094cd70fcf309a93b97b6697595b71ce
ektasrivastava31 <35468369+ektasrivastava31@users.noreply.github.com>
2018-01-18 19:12:37 +0530

    Add files via upload

---
+++ third.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	char n;
+	printf("enter the character\n");
+	scanf("%s",&n);
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+	{
+		printf("vowel");
+	}
+	else
+	printf("consonent");
+}
